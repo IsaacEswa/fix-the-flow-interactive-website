c86ecdb0146efb965d4057364593a361652d04e8
IsaacEswa <isaaceswa@gmail.com>
2025-12-03 14:43:08 +0100

    Verhuizing van alle bestanden

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// 1. Selecteer de button waarmee ik de artikelen op datum sorteer
+let sorteerButton = document.querySelector('.sort-button');
+
+// 2. Wacht tot iemand op die button klikt
+sorteerButton.addEventListener('click', function () {
+    // 3. Sorteer de artikelen op datum (nieuwste eerst) met datetime attribute
+    document.querySelector('.news-cards').classList.toggle('sorted-on-date');
+    sorteerButton.classList.toggle('sorted');
+});
