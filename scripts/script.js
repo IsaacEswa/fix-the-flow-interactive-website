3c88934d61a4f672d4f52d2e368827d43561c530
IsaacEswa <isaaceswa@gmail.com>
2025-12-03 14:45:07 +0100

    Start filter knop op nieuwspagina #8

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
+// Filter knop op nieuwspagina
 // 1. Selecteer de button waarmee ik de artikelen op datum sorteer
+
 let sorteerButton = document.querySelector('.sort-button');
 
 // 2. Wacht tot iemand op die button klikt
